7641d5177cc6d3b0f7b0d24a2e2164dd86a640c6
Eleuterio Ramírez <elramirez@uc.cl>
2019-02-27 13:10:47 -0300

    updating main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import QTimer, pyqtSignal, QObject, QSize, Qt, QThread, \
 from PyQt5.QtWidgets import QLabel, QWidget, QMainWindow, QApplication, \
     QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit, QProgressBar, QGroupBox
 from PyQt5.Qt import QTest, QTransform, QSound
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
 import time
 from selenium import webdriver
 from driver_options import options, chromedriver_path
@@ -16,17 +16,60 @@ class Window(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setGeometry(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.setWindowFlags(Qt.FramelessWindowHint)
-
-        self.web_label = QLabel("Inserte URL de su página web: ", self)
-        self.web_label.setGeometry(10, 10, SCREEN_WIDTH*0.2, SCREEN_HEIGHT*0.05)
-        self.web_name = QLineEdit("", self)
-        self.web_name.setGeometry(SCREEN_WIDTH*0.2+10, 10, SCREEN_WIDTH*0.2, SCREEN_HEIGHT*0.05)
-        self.accept_button = QPushButton("Aceptar", self)
-        self.accept_button.setGeometry(SCREEN_WIDTH*0.4+10, 10, SCREEN_WIDTH*0.2, SCREEN_HEIGHT*0.05)
-        self.accept_button.clicked.connect(lambda: self.openBrowser(self.web_name.text()))
-
+        self.background = QLabel(self)
+        self.background.setGeometry(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.background.setPixmap(QPixmap("images/background.png").scaled(SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.main_layout = QGroupBox(self)
+        self.main_layout.setFixedSize(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.display_main_menu()
         self.show()
+
+    def display_main_menu(self):
+        self.url_main_button = QPushButton("", self.main_layout)
+        self.url_main_button.setGeometry(col1, row3, 4*col, 4*col)
+        self.url_main_button.setIcon(QIcon("images/item1.png"))
+        self.url_main_button.setIconSize(QSize(4*col, 4*col))
+        self.url_main_button.setStyleSheet("background: transparent");
+        self.url_main_button.clicked.connect(lambda: self.hide_main_menu(self.display_url_menu))
+
+        self.url_main_button = QPushButton("", self.main_layout)
+        self.url_main_button.setGeometry(col5, row3, 4*col, 4*col)
+        self.url_main_button.setIcon(QIcon("images/item1.png"))
+        self.url_main_button.setIconSize(QSize(4*col, 4*col))
+        self.url_main_button.setStyleSheet("background: transparent");
+        self.url_main_button.clicked.connect(lambda: self.hide_main_menu(self.display_url_menu))
+
+        self.url_main_button = QPushButton("", self.main_layout)
+        self.url_main_button.setGeometry(col1, row6-100, 4*col, 4*col)
+        self.url_main_button.setIcon(QIcon("images/item1.png"))
+        self.url_main_button.setIconSize(QSize(4*col, 4*col))
+        self.url_main_button.setStyleSheet("background: transparent");
+        self.url_main_button.clicked.connect(lambda: self.hide_main_menu(self.display_url_menu))
+
+        self.url_main_button = QPushButton("", self.main_layout)
+        self.url_main_button.setGeometry(col5, row6-100, 4*col, 4*col)
+        self.url_main_button.setIcon(QIcon("images/item1.png"))
+        self.url_main_button.setIconSize(QSize(4*col, 4*col))
+        self.url_main_button.setStyleSheet("background: transparent");
+        self.url_main_button.clicked.connect(lambda: self.hide_main_menu(self.display_url_menu))
+
+    def hide_main_menu(self, menu):
+        self.main_layout.hide()
+        menu()
+
+
+    def display_url_menu(self):
+        self.url_layout = QGroupBox(self)
+        self.url_layout.setFixedSize(col10, row10)
+        self.web_label = QLabel("Inserte URL de su página web: ", self.url_layout)
+        self.web_label.setGeometry(col2, row3, col, row/2)
+        self.web_name = QLineEdit("", self.url_layout)
+        self.web_name.setGeometry(col4, row3, col*2, row/2)
+        self.accept_button = QPushButton("Aceptar", self.url_layout)
+        self.accept_button.setGeometry(col6, row3, col*2, row/2)
+        self.accept_button.clicked.connect(lambda: self.openBrowser(self.web_name.text()))
+        self.url_layout.show()
+
 
     def openBrowser(self, name):
         print(self.web_name.text())
